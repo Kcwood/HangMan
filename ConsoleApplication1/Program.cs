48010ad013204e92cbbbb3abea92cfd90da9a908
Kyle Wood <kyle.c.wood88@gmail.com>
2014-06-29 00:12:49 -0600

    Hangman
    Working on final code - only partially commented.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + " to the HangMan game.");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("                                HANGMAN GAME              ");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("In this game, you are going to guess the letters of the word I'll give you. You have 5 guesses.");
+            Console.Write("Are you ready? ");
+            string confirm = Console.ReadLine().ToLower();
+            while (confirm != "yes" && confirm != "no")
+            {
+                Console.WriteLine();
+                Console.WriteLine("I don't understand you.");
+                Console.Write("Are you ready? ");
+                confirm = Console.ReadLine().ToLower();
+            }
+            if (confirm == "no")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Well then, see you later...");
+                Console.WriteLine("--------------------------------------------------------------------------");
+                Console.WriteLine("Press any key to exit...");
+            }
+            else if (confirm == "yes")
+            {
+                hangMan();
+            }
+            Console.ReadKey();
+        }
+        static void hangMan()
+        {
+            Random random = new Random();
+            string[] wordBank = { "Blue", "Black", "Yellow", "Orange", "Green", "Purple" };
+            string wordToGuess = wordBank[random.Next(0, wordBank.Length)];
+            string wordToGuessUppercase = wordToGuess.ToUpper();
+            StringBuilder displayToPlayer = new StringBuilder(wordToGuess.Length);
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                displayToPlayer.Append("_");
+            }
+            List<char> correctGuessess = new List<char>();
+            List<char> incorrectGuesses = new List<char>();
+            int lives = 5;
+            bool won = false;
+            int lettersRevealed = 0;
+            string input;
+            char guess;
+            while (!won && lives > 0)
+            {
+                Console.Write("Guess a letter: ");
+                input = Console.ReadLine().ToUpper();
+                if (input.Length > 1)
+                {
+                    if (input == wordToGuessUppercase)
+                    {
+                        won = true;
+                    }
+                    else
+                    {
+                        won = false;
+                        Console.WriteLine("Nope, you're wrong");
+                        lives--;
+                        Console.WriteLine();
+                        Console.WriteLine("Guess left: " + lives);
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    guess = input[0];
+                    if (correctGuessess.Contains(guess))
+                    {
+                        Console.WriteLine("You've already tried " + guess + ", and it was correct!");
+                        Console.WriteLine();
+                        Console.Write("Letter guessed: ");
+                        loop(correctGuessess);
+                        loop(incorrectGuesses);
+                        Console.WriteLine();
+                        Console.WriteLine("Guess left: " + lives);
+                        Console.WriteLine();
+                        continue;
+                    }
+                    else if (incorrectGuesses.Contains(guess))
+                    {
+                        Console.WriteLine("You've already tried " + guess + ", and it was incorrect.");
+                        Console.WriteLine();
+                        Console.Write("Letter guessed: ");
+                        loop(correctGuessess);
+                        loop(incorrectGuesses);
+                        Console.WriteLine();
+                        Console.WriteLine("Guess left: " + lives);
+                        Console.WriteLine();
+                        continue;
+                    }
+                    if (wordToGuessUppercase.Contains(guess))
+                    {
+                        correctGuessess.Add(guess);
+                        for (int i = 0; i < wordToGuess.Length; i++)
+                        {
+                            if (wordToGuessUppercase[i] == guess)
+                            {
+                                displayToPlayer[i] = wordToGuess[i];
+                                lettersRevealed++;
+                                Console.WriteLine();
+                                Console.Write("Letter guessed: ");
+                                loop(correctGuessess);
+                                loop(incorrectGuesses);
+                                Console.WriteLine();
+                                Console.WriteLine("Guess left: " + lives);
+                                Console.WriteLine();
+                            }
+                        }
+                        if (lettersRevealed == wordToGuess.Length)
+                        {
+                            won = true;
+                        }
+                    }
+                    else
+                    {
+                        incorrectGuesses.Add(guess);
+                        Console.WriteLine("Nope, there's no " + guess + " in it!");
+                        lives--;
+                        Console.WriteLine();
+                        Console.Write("Letter guessed: ");
+                        loop(correctGuessess);
+                        loop(incorrectGuesses);
+                        Console.WriteLine();
+                        Console.WriteLine("Guess left: " + lives);
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine(string.Join(" ", displayToPlayer.ToString().ToList()));
+                }
+
+            }
+            if (won)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You won!");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("You lost! It was " + wordToGuess);
+            }
+            Console.WriteLine("Press any key to exit...");
+        }
+        static void loop(List<char> a)
+        {
+            foreach (char b in a)
+            {
+                Console.Write(b + " ");
+            }
+        }
+    }
+}
--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -10,77 +10,178 @@ namespace HangMan
     {
         static void Main(string[] args)
         {
-            HangMan();
-            
-
-
-
-
+            //Starting the game
+            Console.Write("Hey you there! What's your name?");
+            //Getting info from the game player
+            string name = Console.ReadLine();
+            //Telling the player the rules of the game
+            Console.WriteLine("Greetings " + name + ", I am the Doctor. I would like for you to be my companion on an adventure through time and space.");
+            Console.WriteLine("On this adventure you and I will be playing a game. A game that will decide the fate of the universe.");
+            Console.WriteLine("If you choose to accept it, we will attempt to save all of time and space from a horrible race called the Weeping Angels. We will do this by playing a game of hangman. You will guess letters to words that I am thinking and I will help guide you to the correct answer. You only have a limited number of guesses, so be vigilent. All of time and space is depending on you!");
+            //Getting the users answer to whether or not
+            //they would like to play our game
+            string confirm = Console.ReadLine().ToLower();
+            Console.Write("Are you ready to depart in the TARDIS?");
+            confirm = Console.ReadLine().ToLower();
+            //If the users answer is "no" 
+            //the game will end with the following 
+            //printed to the console
+            if (confirm == "no")
+            {
+                Console.WriteLine();
+                Console.WriteLine("The Weeping Angels have no destroyed us all. All of time and space is doomed thanks to you.");
+            }
+            //If the user answers "yes"
+            //then the game will start
+            else if (confirm == "yes")
+            {
+                HangMan();
+            }
+            //Keeps the console open
             Console.ReadKey();
         }
-        //Setting up the function for HangMan
         static void HangMan()
         {
-            
-            //Getting the users name
-            Console.WriteLine("Hey you! What is your name?");
-            Console.WriteLine();
-            //Allowing the user to interact with the computer
-            string name = Console.ReadLine();
-            Console.WriteLine();
-            //Greeting the user and explaining the rules 
-            //and asking the user if they would like to play
-            Console.WriteLine("Greetings fellow traveler " + name + "! I am the Doctor. I would like to play a game called Hangman with you. The rules are simple. I will make a word and you will guess a letter to see if it is in the word that I have made. If so, I will unveil those letters to you and you will continue guessing until you have run out of guesses or you have guessed the word. If you guess the word correctly, you will have had a grand adventure through time and space. If you don't, the Angels will have zapped you permantently to another time and you will die a slow death! Would you like to play?");
-            Console.WriteLine();
-            //Allowing the user to interact with the computer
-            string gamePlay = Console.ReadLine();
-            Console.WriteLine();
-            //Getting the users answer
-            while (gamePlay == "yes" || gamePlay == "Yes" || gamePlay == "y")
+            //Setting up the random word generator
+            //with a list of words of my choosing
+            Random wordRandom = new Random();
+            string[] wordBank = { "geronimo", "fezzes", "bowties", "dalek", "sweetie", "screwdriver", "custard", "fantastic"};
+            string wordToGuess = wordBank[wordRandom.Next(0, wordBank.Length)];
+            //Making it so if the user enters in 
+            //lowercase or uppercase letters 
+            //the game will still function
+            string wordToGuessUpperCase = wordToGuess.ToUpper();
+            StringBuilder displayToPlayer = new StringBuilder(wordToGuess.Length);
+            //For the length of the randomly 
+            //choosen word it will display "_"
+            for (int i = 0; i < wordToGuess.Length; i++)
             {
-                //Starting the game
-                Console.WriteLine("Alright! Let's hop in the TARDIS and off we go! Geronimo!");
-                Console.WriteLine();
-                Console.WriteLine("I have thought of a word. Make your first guess!");
-                //Making a list of words to generate
-                string [] wordList = new string [] { "badwolf", "spoilers", "gallifrey", "custard", "pandorica", "ood", "timelash", "dalek", "cybermen", "rose", "martha", "clara", 
-                "amy","rory", "mickie", "donna", "titanic", "screwdriver" };
-                int i = 0; 
-                //setting up the random word generator
-                //to find an index of the word list 
-                //we created
-                Random word = new Random();
-                word.Next(0, 17);
-                string randomWord = wordList[i];
-                //Creating some variables for our 
-                //while loop
-                int totalGuess = 10;
-                int noGuess = 0;
-                while (totalGuess > noGuess)
-                {
-
-                    
-                }
-
-
-                
-
-                
+                displayToPlayer.Append("_");
             }
-
+            //Setting up to count the number of 
+            //correct and incorrect guesses
+            List<char> correctGuesses = new List<char>();
+            List<char> incorrectGuesses = new List<char>();
+            //Allowing the user to only have 
+            //10 attempts at guessing the word
+            int lives = 10;
+            bool won = false;
+            int letterRevealed = 0;
+            string input;
+            char guess;
+            while (!won && lives > 0)
+            {
+                Console.Write("I have a word in mind. What is your guess?");
+                input = Console.ReadLine().ToUpper();
+                if (input.Length > 1)
+                {
+                    if (input == wordToGuessUpperCase)
+                    {
+                        won = true;
+                    }
+                    else
+                    {
+                        won = false;
+                        Console.WriteLine("Rats, your guess was incorrect. Please try another letter.");
+                        lives--;
+                        Console.WriteLine();
+                        Console.WriteLine("You have " + lives + " guess left to save all of time and space!");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    guess = input[0];
+                    if (correctGuesses.Contains(guess))
+                    {
+                        Console.WriteLine("You have already attempted that " + guess);
+                        Console.WriteLine();
+                        Console.Write("These are the letters you have already tried: ");
+                        loop(correctGuesses);
+                        loop(incorrectGuesses);
+                        Console.WriteLine();
+                        Console.WriteLine("You have " + lives + " guesses left companion. All of space and time is counting on you.");
+                        Console.WriteLine();
+                        continue;
+                    }
+                    else if (incorrectGuesses.Contains(guess))
+                    {
+                        Console.WriteLine("I am sorry but " + guess + " was attempted and it was incorrect.");
+                        Console.WriteLine();
+                        Console.Write("These are the letters you have already tried: ");
+                        loop(correctGuesses);
+                        loop(incorrectGuesses);
+                        Console.WriteLine();
+                        Console.WriteLine("You have " + lives + " guesses left companion. All of space and time is counting on you.");
+                        Console.WriteLine();
+                        continue;
+                    }
+                    if (wordToGuessUpperCase.Contains(guess))
+                    {
+                        correctGuesses.Add(guess);
+                        for (int i = 0; i < wordToGuess.Length; i++)
+                        {
+                            if (wordToGuessUpperCase[i] == guess)
+                            {
+                                displayToPlayer[i] = wordToGuess[i];
+                                letterRevealed++;
+                                Console.WriteLine();
+                                Console.Write("These are the letters you have already tried: ");
+                                loop(correctGuesses);
+                                loop(incorrectGuesses);
+                                Console.WriteLine();
+                                Console.WriteLine("You have " + lives + " guesses left companion. All of space and time is counting on you.");
+                                Console.WriteLine();
+                            }
+                        }
+                        if (letterRevealed == wordToGuess.Length)
+                        {
+                            won = true;
+                        }
+                    }
+                    else
+                    {
+                        incorrectGuesses.Add(guess);
+                        Console.WriteLine("I am sorry but " + guess + " was attempted and it was incorrect.");
+                        lives--;
+                        Console.WriteLine();
+                        Console.Write("These are the letters you have already tried: ");
+                        loop(correctGuesses);
+                        loop(incorrectGuesses);
+                        Console.WriteLine();
+                        Console.WriteLine("You have " + lives + " guesses left companion. All of space and time is counting on you.");
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine(string.Join(" ", displayToPlayer.ToString().ToList()));
+                }
+            }
+            //If the user has succeed in guessing the word that 
+            //that was generated it will print the following
+            //and the game will end
+            if (won)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You have saved all of time and space! You are by far the best companion that I have ever had the please of having!");
+            }
+            //If the user did not guess the correct word
+            //in the amount of guesses given then
+            //this will print and the game will end
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("The Weeping Angels got us in the end. You shall now die a slow death and you have doomed all of time and space.");
+            }
         }
-        static void MaskedWord()
+        //Setting up a loop to help the 
+        //lives counter run correctly throughout
+        //the game
+        static void loop(List<char> a)
         {
-            //Making a list of words to generate
-            string[] wordList = new string[] { "badwolf", "spoilers", "gallifrey", "custard", "pandorica", "ood", "timelash", "dalek", "cybermen", "rose", "martha", "clara", 
-                "amy","rory", "mickie", "donna", "titanic", "screwdriver" };
-            int i = 0;
-            //setting up the random word generator
-            //to find an index of the word list 
-            //we created
-            Random word = new Random();
-            word.Next(0, 17);
-            string randomWord = wordList[i];
+            foreach (char b in a)
+            {
+                Console.Write(b + " ");
+            }
         }
     }
 }
+
